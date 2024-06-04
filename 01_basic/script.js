b3ea1e4a8d933b92e143e1a99a1e9b3936dac0ba
Chandra583 <chandrashekhargawda2000@gmail.com>
2024-06-04 14:06:40 +0530

    string number in console

--- 01_basic/script.js
+++ 01_basic/script.js
@@ -1,25 +1,37 @@
 //data type conversion or operation
 
-let age = "20";
+// let age = "20";
 
-let valueInNum = Number(age);
+// let valueInNum = Number(age);
 
-console.log(typeof valueInNum);
+// console.log(typeof valueInNum);
 
-//33 => 33
-//"33abc" => NaN
+// //33 => 33
+// //"33abc" => NaN
 
-let isLogin = "";
+// let isLogin = "";
 
-let isBoolean = Boolean(isLogin);
-console.log(isBoolean);
+// let isBoolean = Boolean(isLogin);
+// console.log(isBoolean);
 
 //1 => true
 //0 => false
 //when its a string like "name/any charater" => ture
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let toString = String(someNumber);
-console.log(toString);
-console.log(typeof toString);
+// let toString = String(someNumber);
+// console.log(toString);
+// console.log(typeof toString);
+
+//  ********operation********
+
+let str1 = "heloo";
+let str2 = "wrold";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(1 + 2);
+console.log("1" + 2 + 3);
+console.log(1 + "2" + 3);
