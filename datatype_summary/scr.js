4530a2b78420f368ae86fda407a90ec2be7ba8ef
Chandra583 <chandrashekhargawda2000@gmail.com>
2024-06-05 23:24:19 +0530

    strings

---
+++ datatype_summary/scr.js
@@ -0,0 +1,7 @@
+let myName = "chandrashekar";
+let repoCount = 55;
+let myUserName = "chandra583";
+
+console.log(
+  `my name is ${myName} my github userName is ${myUserName} and total repocounts are ${repoCount}`
+);
